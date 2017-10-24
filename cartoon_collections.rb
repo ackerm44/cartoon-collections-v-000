dd140ac15ed79b6fffdac97d8f9fd4b71c98d828
Amanda Ackerman <ackerm44@gmail.com>
2017-10-24 02:34:46 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,8 +14,8 @@ def long_planeteer_calls(array)
   array.any? {|word| word.length > 4}
 end
 
-def find_the_cheese(array)
-  cheese_types = ["cheddar", "gouda", "camembert"]
-   if array.include?(cheese_types.any?)
-     return cheese_types
-end
+#def find_the_cheese(array)
+  #cheese_types = ["cheddar", "gouda", "camembert"]
+  # if array.include?(cheese_types.any?)
+    # return cheese_types
+#end
